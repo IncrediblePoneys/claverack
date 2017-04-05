03160b911f5da665b18f6e524ab195433de394a4
Alexandre Gaudencio <alexandre.gaudencio@ext.euronews.com>
2017-04-06 00:14:56 +0200

    Fix indentation

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
 				</div>
 				<p className="App-intro">
 					To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+				</p>
 			</div>
 		)
 	}
