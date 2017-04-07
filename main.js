ff4c26cf6f7816b807e4ff0d6c9b5e0b772b0203
Alexandre Gaudencio <alexandre.gaudencio@ext.euronews.com>
2017-04-07 02:12:33 +0200

    Working auth + small stuff
    The user is able to login and get his timeline

--- main.js
+++ main.js
@@ -15,7 +15,13 @@ let mainWindow
 
 function createWindow() {
 	// Create the browser window.
-	mainWindow = new BrowserWindow({ width: 800, height: 600 })
+	mainWindow = new BrowserWindow({
+		width: 800,
+		height: 600,
+		webPreferences: {
+			webSecurity: false
+		}
+	})
 
 	const startUrl = process.env.ELECTRON_START_URL || url.format({
 		pathname: path.join(__dirname, '/../build/index.html'),
--- src/App.js
+++ src/App.js
@@ -1,20 +1,100 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
+import { registerApp, login, timeline } from './utils/api'
+
 class App extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			credentials: null,
+			timeline : null
+		}
+
+		this._handleSubmit = this.handleSubmit.bind(this)
+	}
+
+	componentDidMount() {
+		Promise.resolve(localStorage.getItem('credentials'))
+			.then(credentials => {
+				if (!credentials) {
+					return registerApp()
+						.then(JSON.stringify)
+						.then(credentials => {
+							localStorage.setItem('credentials', credentials)
+							return credentials
+						})
+				}
+				return credentials
+			})
+			.then(credentials => {
+				this.setState(() => {
+					return {
+						credentials
+					}
+				})
+			})
+	}
+
+	handleSubmit (e) {
+		e.preventDefault()
+
+		const { client_id, client_secret } = JSON.parse(this.state.credentials)
+
+		let data = new FormData(e.target)
+		data.append('client_id', client_id)
+		data.append('client_secret', client_secret)
+		data.append('grant_type', 'password')
+
+		login(data)
+			.then(timeline)
+			.then((timeline) => {
+				this.setState(() => ({
+					timeline
+				}))
+			})
+	}
+
 	render() {
-		return (
-			<div className="App">
-				<div className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<h2>Welcome to React</h2>
-				</div>
-				<p className="App-intro">
-					To get started, edit <code>src/App.js</code> and save to reload.
-				</p>
+		const { credentials, timeline } = this.state
+
+		if (!credentials) {
+			return <div>
+				Loading...
 			</div>
-		)
+		}
+
+		if (!timeline) {
+			return <form onSubmit={this._handleSubmit}>
+				<p>
+					<label htmlFor="username">Username</label>
+					<input id="username" type="text" name="username" />
+				</p>
+				<p>
+					<label htmlFor="password">Password</label>
+					<input id="password" type="password" name="password" />
+				</p>
+				<p>
+					<label htmlFor="instance">Instance</label>
+					<input id="instance" type="text" name="instance" value="https://mastodon.social"/>
+				</p>
+				<input type="submit" value="Submit" />
+			</form>
+		}
+
+		return <section>
+			{timeline.map((toot, index) => {
+				const content = (toot.reblog && toot.reblog.content) || toot.content
+
+				return <div key={index}>
+					<div>
+						{content}
+					</div>
+					<hr/>
+				</div>
+			})}
+		</section>
 	}
 }
 
