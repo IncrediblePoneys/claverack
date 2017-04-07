ec8d9676896eb7abc1aaf3e994fcdf660172b429
Alexandre Gaudencio <shahor@shahor.fr>
2017-04-07 16:42:40 +0200

    Store users

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -3,9 +3,13 @@ import {
 	LOGIN
 } from '../constants/users'
 
+const USERS_KEY = 'users'
+
+const users = JSON.parse(localStorage.getItem(USERS_KEY)) || {}
+
 const initialState = {
-	selected: null,
-	accounts: []
+	selected: users.selected === undefined ? null : users.selected,
+	accounts: users.accounts || []
 }
 
 export default (state = initialState, action) => {
@@ -17,11 +21,19 @@ export default (state = initialState, action) => {
 			}
 		case LOGIN:
 			let accounts = state.accounts.concat(action.user)
-			return {
+
+			const newState = {
 				...state,
 				accounts,
 				selected : accounts.length - 1
 			}
+
+			localStorage.setItem(USERS_KEY, JSON.stringify({
+				accounts : newState.accounts,
+				selected : newState.selected
+			}))
+
+			return newState
 		default:
 			return state
 	}
