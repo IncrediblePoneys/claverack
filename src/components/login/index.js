3f05494fc90bd3e124ea38f23bccd4dfad37b748
Alexandre Gaudencio <shahor@shahor.fr>
2017-04-11 01:05:07 +0200

    Add a way to get the instances list
    There's no official api for that so ... 💩  ensues
    html5 autocomplete doesn't work on electron :(
    We'll have to find another way, but it doesn't prevent from normal usage :)

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react'
 import { translate } from 'react-i18next'
 import PropTypes from 'prop-types'
+import { getInstances } from '../../utils/instances'
 
 class Login extends Component {
 	constructor (props) {
 		super(props)
 		this.handleSubmit = this.handleSubmit.bind(this)
+		this.state = {
+			loading : true,
+			instances : [],
+			suggestions : []
+		}
+	}
+
+	async componentDidMount () {
+		const instances = await getInstances()
+		this.setState({
+			loading: false,
+			instances
+		})
 	}
 
 	handleSubmit (e) {
@@ -17,6 +31,13 @@ class Login extends Component {
 
 	render () {
 		const { t } = this.props
+		const { loading, instances } = this.state
+
+		if (loading) {
+			return <div>
+				Loading...
+			</div>
+		}
 
 		return <form onSubmit={this.handleSubmit}>
 			<p>
@@ -35,7 +56,18 @@ class Login extends Component {
 				<label htmlFor="instance">
 					{t('instance')}
 				</label>
-				<input id="instance" type="text" name="instance" value="https://mastodon.social" />
+				<input
+					name="instance"
+					list="instances"
+					value="https://mastodon.social"
+				/>
+				<datalist id="instances">
+					{instances.map((instance, index) => {
+						return <option key={index}>
+							{instance}
+						</option>
+					})}
+				</datalist>
 			</p>
 			<button type="submit">
 				{t('submit')}
---
+++ src/utils/instances.js
@@ -0,0 +1,31 @@
+// Word of ⚠️
+// If you are under age or don't like seeing ugly things, please don't read this file.
+// thanks, ❤️
+
+// but really though, there's no API for that right now so I decided to go the dirty way.
+
+async function getIt () {
+	// 😂
+	const API = "https://instances.mastodon.xyz/"
+	return fetch(API)
+		.then(response => response.text())
+}
+
+	// This is where the 💩 happens
+export async function getInstances() {
+	// Can you see it coming?
+	const node = document.createElement('div')
+	// oh yeah 💃
+	node.innerHTML = await getIt()
+	// I just did that 😎
+	const links = [...node.querySelectorAll('a')]
+
+	// sometimes, dirty way is the good way 🍆
+	return links.reduce((accumulator, link) => {
+		if (!/localhost/.test(link)) {
+			accumulator.push(link.href)
+		}
+
+		return accumulator
+	}, [])
+}
