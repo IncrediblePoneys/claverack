fabb363a4c9b4d3fb3d739a6e9e056670327abff
Alexandre Gaudencio <shahor@shahor.fr>
2017-04-11 13:18:31 +0200

    There's an API after all

--- src/utils/instances.js
+++ src/utils/instances.js
@@ -1,31 +1,13 @@
-// Word of ⚠️
-// If you are underage or don't like seeing ugly things, please don't read this file.
-// thanks, ❤️
-
-// but really though, there's no API for that right now so I decided to go the dirty way.
-
-async function getIt () {
-	// 😂
-	const API = "https://instances.mastodon.xyz/"
+async function getIt() {
+	const API = "https://instances.mastodon.xyz/instances.json"
 	return fetch(API)
-		.then(response => response.text())
+		.then(response => response.json())
 }
 
-	// This is where the 💩 happens
 export async function getInstances() {
-	// Can you see it coming?
-	const node = document.createElement('div')
-	// oh yeah 💃
-	node.innerHTML = await getIt()
-	// I just did that 😎
-	const links = [...node.querySelectorAll('a')]
+	const instances = await getIt()
 
-	// sometimes, dirty way is the good way 🍆
-	return links.reduce((accumulator, link) => {
-		if (!/localhost/.test(link)) {
-			accumulator.push(link.href)
-		}
-
-		return accumulator
-	}, [])
+	return instances
+		.slice(0, 30)
+		.map(instance => `https://${instance.name}`)
 }
